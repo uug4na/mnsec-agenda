ae224472c78bad16818725840f0dc2273c2c3eb0
uug4na <uug4na@gmail.com>
2024-09-25 19:26:39 +0800

    location qr removed

--- src/app/sections/agendaBackground.tsx
+++ src/app/sections/agendaBackground.tsx
@@ -1,6 +1,11 @@
 const Agendabackground = () => {
     return (
-        <div className="bg-[url('/bg-3.svg')] bg-cover bg-center min-h-screen h-screen">
+        <div className="bg-[url('/bg-3.svg')] bg-cover bg-center h-screen flex justify-center">
+            <div className="max-w-[1126px]">
+                <div className="font-semibold text-lg md:text-4xl lg:text-6xl mt-20 mx-20 self-center text-[#B1D572]">
+                    “There Is A Difference Between Knowing The Path And Walking The Path.”
+                </div>
+            </div>
         </div>
     )
 }
